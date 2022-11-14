60cbcc2f04a9c7489d69f9d7ff8444ed718b35d8
DonaldoG93 <germenjid@gmail.com>
2022-11-14 18:25:59 +0100

    .

--- Project1/test.js
+++ Project1/test.js
@@ -14,10 +14,9 @@ function factorial (a){
  if(a == 0 || a == 1){
     return 1;
  } else{
-    return a *factorial (a-1));
-
-    }
+    return a *factorial (a-1);
+        }
 }
 
-factorial(5);
+console.log(factorial(5))
 
