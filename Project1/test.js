544d89a221d26064a39f04993d766ec6804890ae
DonaldoG93 <germenjid@gmail.com>
2022-11-14 18:24:28 +0100

    done

--- Project1/test.js
+++ Project1/test.js
@@ -1,2 +1,23 @@
-var a = 20;
+// power function 
+
+function power (a, b){
+   let c= a**b;
+   console.log(c); 
+}
+
+
+power(2,3);
+
+
+// function factorial  
+function factorial (a){
+ if(a == 0 || a == 1){
+    return 1;
+ } else{
+    return a *factorial (a-1));
+
+    }
+}
+
+factorial(5);
 
