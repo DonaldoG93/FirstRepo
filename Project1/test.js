416e75aa1ca5634b50844e1ebe1d32776ad3347b
DonaldoG93 <germenjid@gmail.com>
2022-11-16 22:04:44 +0100

    .recursion fair enough

--- Project1/test.js
+++ Project1/test.js
@@ -1,12 +1,12 @@
-// power function 
+// // power function 
 
-function power (a, b){
-   let c= a**b;
-   console.log(c); 
-}
+// function power (a, b){
+//    let c= a**b;
+//    console.log(c); 
+// }
 
 
-power(2,3);
+// power(2,3);
 
 
 // function factorial  
@@ -21,29 +21,42 @@ function factorial (a){
 console.log(factorial(5));
 
 
-RUN factorial(5)
+// RUN factorial(5)
 
-1 is (a == 0 || a == 1) => No  
+// 1 is (a == 0 || a == 1) => No  
 
-   then return a *factorial (5-1);  /*a=4 */
+//    then return a *factorial (5-1);  /*a=4 */
+//  ///           5 * factorial (4) 
+// 2 is (a == 0 || a == 1) => No  
+//    then return  a *factorial (4-1);  /*a=3 */
+// ///             4 * factorial (3) 
 
-2 is (a == 0 || a == 1) => No  
-   then return  a *factorial (4-1);  /*a=3 */
+// 3 is (a == 0 || a == 1) => No  
+//    then return  a *factorial (3-1);  /*a=2 */
+//    ///             3 * factorial (2) 
+
+// 4 is (a == 0 || a == 1) => No  
+//    then return  a *factorial (2-1);  /*a=1 */
+//  ///             2 * factorial (1)   
+
+// 5 is (a == 0 || a == 1) => Yes  
+//    then return  1 
 
 
-3 is (a == 0 || a == 1) => No  
-   then return  a *factorial (3-1);  /*a=2 */
-   
+//    // BACKWAY (stack exec)
 
-4 is (a == 0 || a == 1) => No  
-   then return  a *factorial (2-1);  /*a=1 */
+// 5. factorial (1) = 1 /* from base statement*/
+
+// 4. 2 * factorial (1) => 2* 1 = 2    /*  facttorial(2) = 2 */
+
+// 3. 3 * factorial(2) = 3*2 = 6  /* factorial(3) = 6 */
+
+// 2. 4 * factorial (3) = 4 *6  /*factorial(4) = 24 */
+
+// 1. 5 * factorial (4) = 5 * 24 = 120
 
 
-5 is (a == 0 || a == 1) => Yes  
-   then return  1 
 
-
-   // BACKWAY
 
 
 
