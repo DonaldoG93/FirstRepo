deda08528cf6361b3f066ac75919012dbae9868e
DonaldoG93 <germenjid@gmail.com>
2023-02-15 17:09:39 +0100

    whatever

--- JS Lections/Promises.js
+++ JS Lections/Promises.js
@@ -16,21 +16,60 @@
 
 // setInterval(()=> {console.log(Math.random())},10) 
 
-let mreti = (...args) => {
+// let mreti = (...args) => {
 
-    return new Promise ((po, jo) =>{
+//     return new Promise ((po, jo) =>{
 
-        let s=0
-        for (let i=0; i<= args.length; i++){
-            s+=args[i] }
+//         let s=0
+//         for (let i=0; i< args.length; i++){
+//             s+=args[i]}
 
-            if(s%2==0)
-            po(args)
-            else jo("Array ka dhe elemente tek") 
+//             if(s%2==0)
+//             po(args)
+//             else jo("tek dak") 
        
-    }
+//     }
         
-    )
-}
+//     )
+// }
 
-mreti(2,4,6,10,10).then(po => console.log(po)).catch(jo => console.log(jo))
+// mreti(2,4,6,10,10).then(po => console.log(po)).catch(jo => console.log(jo))
+
+// let mreti = async (...args) => {
+
+//     const princi = new Promise ((po, jo) =>{
+
+//         let s=0
+//         for (let i=0; i< args.length; i++){
+//             s+=args[i]}
+
+//             if(s%2==0)
+//             po(args)
+//             else jo("tek dak") 
+       
+//     }); 
+//     console.log(await princi)
+// }
+// mreti(2,52,6,10,10).catch(error=> console.log(error))
+
+
+// let p = new Promise ((resolved, reject) => {
+//     console.log("presim te futet a=ja")
+//     let a =setTimeout(() =>  4, 6000);
+//         if (a==4){
+//         resolved("succes")
+//     } else {
+//         reject("Failed")
+//     }
+//     }
+//     )
+
+
+// p.then(detail => console.log("po plak") ).catch(error => console.log("po jari po"))
+
+let a =setTimeout(function ()return 4, 4000)
+
+console.log(a())
+
+
+ 
